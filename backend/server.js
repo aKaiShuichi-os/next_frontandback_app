9bc18cdbd26c414812270ab91adb55a4b2f7291c
aKaiShuichi-os <chhabravinayak06@gmail.com>
2025-02-03 15:49:02 +0530

    reset

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+//default route
+app.get("/", (req, res) => {
+  try {
+    res.send("Backend is running....YAY");
+  } catch (err) {
+    console.log("OH NO .... ERROR")
+  }
+  return true;
+});
+
+// MongoDB Connection
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
