2e472ace2759654267700f3647ba493a07c6a22c
aKaiShuichi-os <chhabravinayak06@gmail.com>
2025-02-05 13:53:36 +0530

    deleted all previous commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 // import React from "react";
 
 function App() {
-  return (<div>Namaskaram! Build err is comming</div>);
+  return (<div>Namaskaram! Build your app here</div>);
 }
 
 export default App;
